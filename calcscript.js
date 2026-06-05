3e3c56763d26bc5a02e6887e0fae5131c049a02c
Rushikesh Pandharkar <150378348+Rushikesh-Pandharkar@users.noreply.github.com>
2026-06-06 00:44:52 +0530

    handling number done some modification still remaining

--- calcscript.js
+++ calcscript.js
@@ -1,5 +1,6 @@
 let buffer = "0";
 const screen = document.querySelector(".screen");
+let runningTotal = 0;
 
 init();
 
@@ -33,5 +34,59 @@ function rerender() {
 }
 
 function handleSymbol(value){
-    
+    switch(value) {
+        case "C":
+            buffer = "0";
+            runningTotal = 0;
+            break;
+        case "←":
+            if(buffer.length === 1){
+                buffer = "0";
+            }else{
+                buffer = buffer.substring(0,buffer.length-1);
+            }
+            break;
+        case "=":
+            if(previousOperator === null){
+                return ; //needs two numbers to do the maths
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = +runningTotal;
+            runningTotal = 0;
+            break;
+        case "+":
+        case "-":
+        case "×":
+        case "÷":
+            handleMath(value);
+            break;
+    }
 }
+
+function handleMath(value){
+    if(buffer === "0"){
+        return;//do nothing
+    }
+    const intBuffer = parseInt(buffer);
+    if(runningTotal === 0){
+        runningTotal = intBuffer;
+    }else{
+        flushOperation(intBuffer);
+    }
+    previousOperator = value;
+    buffer = "0";
+}
+
+function flushOperation(intBuffer){
+    if(previousOperator === "+"){
+        runningTotal += intBuffer;
+    }else if(previousOperator === "-"){
+        runningTotal -= intBuffer;
+    }else if(previousOperator === "×"){
+        runningTotal *= intBuffer;
+    }else {
+        runningTotal /= intBuffer;
+    }
+}
+
