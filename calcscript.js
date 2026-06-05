19123c2e2c736aa7eac5c020036c5e0ab694c987
Rushikesh Pandharkar <150378348+Rushikesh-Pandharkar@users.noreply.github.com>
2026-06-05 23:42:52 +0530

    number handling in calc done

--- calcscript.js
+++ calcscript.js
@@ -1,1 +1,37 @@
-console.log("Hello from the calculator");
+let buffer = "0";
+const screen = document.querySelector(".screen");
+
+init();
+
+function init() {
+    document
+        .querySelector(".calc-buttons")
+        .addEventListener("click",(event)=>{
+            buttonClick(event.target.innerText);
+    });
+}
+
+function buttonClick(value) {
+    if(isNaN(parseInt(value))){
+        handleSymbol(value);
+    }else{
+        handleNumber(value);
+    }
+    rerender();
+}
+
+function handleNumber(value){
+    if(buffer === "0"){
+        buffer = value;
+    }else{
+        buffer += value;
+    }
+}
+
+function rerender() {
+    screen.innerText = buffer;
+}
+
+function handleSymbol(value){
+    
+}
